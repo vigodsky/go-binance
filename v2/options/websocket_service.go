ccbb4e50f9bf06f5f3c0dc3c672a3d950a06f3e5
adshao <tjusgj@gmail.com>
2025-04-07 11:40:36 -0300

    Do not close ws conn from client side.
    Binance does not gurantee to send PING messages every 20s,
    currently it sends PING every 3m, we should not close ws conn
    with timeout less than the interval between binance sends PINGs.
    May increase this timeout or remove it in the future.
    
    Signed-off-by: adshao <tjusgj@gmail.com>

--- v2/options/websocket_service.go
+++ v2/options/websocket_service.go
@@ -18,7 +18,7 @@ const (
 
 var (
 	// WebsocketTimeout is an interval for sending ping/pong messages if WebsocketKeepalive is enabled
-	WebsocketTimeout = time.Second * 60
+	WebsocketTimeout = time.Second * 600
 	// WebsocketPongTimeout is an interval for sending a PONG frame in response to PING frame from server
 	WebsocketPongTimeout = time.Second * 10
 	// WebsocketKeepalive enables sending ping/pong messages to check the connection stability
